e82f847fb3b27173c92ccccb85b3799c1d22be48
Mohd Hidayat <hidayat@pernec.com.my>
2024-03-13 14:13:26 +0800

    Chanfe Header for all relative path

--- resources/js/Components/service-details/index.vue
+++ resources/js/Components/service-details/index.vue
@@ -1,14 +1,14 @@
 <script setup>
-import HeaderOne from "@/Layouts/headers/HeaderOne.vue";
 import Breadcrumb from "../common/Breadcrumb.vue";
 import ServiceDetailsArea from "./ServiceDetailsArea.vue";
 import FooterContact from "../portfolio/FooterContact.vue";
 import CommonFooter from "@/Layouts/footers/CommonFooter.vue";
+import HeaderThree from "@/Layouts/headers/HeaderThree.vue";
 
 </script>
 
 <template>
-  <HeaderOne />
+  <HeaderThree />
   <main>
     <Breadcrumb title="Machine Learning" page_title="Service Details" />
     <ServiceDetailsArea />
