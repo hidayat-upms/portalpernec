a289c1ffd9375d6b395bad3ff6b316ab4c190ad0
Mohd Hidayat <hidayat@pernec.com.my>
2024-03-13 09:01:32 +0800

    Done routing 100%

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,25 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return Inertia::render('App', 
-    // [
-    //     'canLogin' => Route::has('login'),
-    //     'canRegister' => Route::has('register'),
-    //     'laravelVersion' => Application::VERSION,
-    //     'phpVersion' => PHP_VERSION,
-    // ]
 );
 });
 
-Route::get('/contact', function () {
-    return Inertia::render('Contact',);
-});
-Route::get('/about', function () {
-    return Inertia::render('About',);
+Route::get('/', function () {
+    return Inertia::render('App', 
+);
 });
 
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
