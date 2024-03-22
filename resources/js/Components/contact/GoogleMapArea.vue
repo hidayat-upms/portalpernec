2e2eebd376c4542c35fb739c123c1281bb07b918
Adry Azman <m.adry@pernec.com.my>
2024-03-22 08:42:50 +0800

    contact us

--- resources/js/Components/contact/GoogleMapArea.vue
+++ resources/js/Components/contact/GoogleMapArea.vue
@@ -3,7 +3,7 @@
 	<div class="tp-contact-map-area">
 		<div class="tp-contact-map">
 			<iframe
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.6789743509316!2d101.73980987567593!3d3.1789070529675323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37764ff7e1d3%3A0x5ec781d1bc95aded!2sPernec%20Corporation%20Berhad!5e0!3m2!1sen!2smy!4v1710213136777!5m2!1sen!2smy"
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.6789950341454!2d101.73980451175461!3d3.1789016967830146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc37764ff7e1d3%3A0x5ec781d1bc95aded!2sPernec%20Corporation%20Berhad!5e0!3m2!1sen!2smy!4v1710303433891!5m2!1sen!2smy"
 				style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="600"
 				height="450"></iframe>
 		</div>
@@ -13,3 +13,5 @@
 <script setup>
 
 </script>
+
+
