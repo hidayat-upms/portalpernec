12a91eb051b139c016b53043f07997cfc2efc4b4
Anatassia <apple@Apples-MacBook-Pro.local>
2024-03-25 12:09:44 +0800

    update footer page

--- resources/js/Components/blog-details/whistleblowing/Indexwhistle.vue
+++ resources/js/Components/blog-details/whistleblowing/Indexwhistle.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Breadcrumb from "@/Components/common/Breadcrumb.vue";
 import Whistleblowing from "@/Components/blog-details/whistleblowing/Whistleblowing.vue";
-import FooterContact from "@/Components/portfolio/FooterContact.vue";
 import CommonFooter from "@/Layouts/footers/CommonFooter.vue";
 import HeaderThree from "@/Layouts/headers/HeaderThree.vue";
 </script>
@@ -11,7 +10,6 @@ import HeaderThree from "@/Layouts/headers/HeaderThree.vue";
   <main>
     <Breadcrumb title="Whistleblowing" page_title="Whistleblowing" />
     <Whistleblowing />
-    <FooterContact />
   </main>
   <CommonFooter />
 </template>
