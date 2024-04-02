620e12b1e8babd227f35ef383319402482b68fd7
hidayat-upms <hidayat@pernec.com.my>
2024-04-02 13:24:24 +0800

    Post deployment

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,39 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: '/resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: [
-          {
-            find: "@",
-            replacement: fileURLToPath(new URL("/resources/js", import.meta.url)),
-          },
-          // '@': fileURLToPath(new URL('./src', import.meta.url)),
-          // '~': ''
-          {
-            find: /^~.+/,
-            replacement: (val) => {
-              return val.replace(/^~/, "");
-            },
-          },
-        ],
+  plugins: [
+    laravel({
+      input: '/resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
       },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("/resources/js", import.meta.url)),
+      },
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // '~': ''
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    chunkSizeWarningLimit: 1024 * 500,
+    minify: true,
+    sourcemap: false,
+  }
 });
