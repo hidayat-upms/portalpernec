38e10cec03ba35c9bf92c9c73b7ab17f7aaaa2bd
Anatassia <apple@Apples-MacBook-Pro.local>
2024-04-02 14:36:54 +0800

    client

--- resources/js/Components/homes/home-2/BrandHomeTwo.vue
+++ resources/js/Components/homes/home-2/BrandHomeTwo.vue
@@ -1,18 +1,4 @@
 <script setup>
-import shape_bg from "@/assets/img/brand/home-2/shape-bg.png";
-import { ref } from 'vue';
-
-const paused = ref(false);
-
-const stopMarquee = () => {
-  paused.value = true;
-};
-
-const startMarquee = () => {
-  paused.value = false;
-};
-
-
 import brand_img_1 from "@/assets/img/brand/home-2/client1.svg";
 import brand_img_2 from "@/assets/img/brand/home-2/client2.svg";
 import brand_img_3 from "@/assets/img/brand/home-2/client3.svg";
@@ -38,86 +24,69 @@ import brand_img_22 from "@/assets/img/brand/home-2/17.png";
 import brand_img_23 from "@/assets/img/brand/home-2/18.png";
 import brand_img_24 from "@/assets/img/brand/home-2/19.png";
 import brand_img_25 from "@/assets/img/brand/home-2/20.png";
-// import brand_img_26 from "@/assets/img/brand/home-2/21.png";
-// import brand_img_27 from "@/assets/img/brand/home-2/22.png";
-// import brand_img_28 from "@/assets/img/brand/home-2/23.png";
-// import brand_img_66 from "@/assets/img/brand/home-2/24.png";
-// import brand_img_29 from "@/assets/img/brand/home-2/25.png";
-// import brand_img_30 from "@/assets/img/brand/home-2/26.png";
-// import brand_img_31 from "@/assets/img/brand/home-2/27.png";
-// import brand_img_32 from "@/assets/img/brand/home-2/28.png";
-// import brand_img_33 from "@/assets/img/brand/home-2/29.png";
-// import brand_img_34 from "@/assets/img/brand/home-2/30.png";
-// import brand_img_35 from "@/assets/img/brand/home-2/31.png";
-// import brand_img_36 from "@/assets/img/brand/home-2/32.png";
-// import brand_img_37 from "@/assets/img/brand/home-2/33.png";
-// import brand_img_38 from "@/assets/img/brand/home-2/34.png";
-// import brand_img_39 from "@/assets/img/brand/home-2/35.png";
-// import brand_img_40 from "@/assets/img/brand/home-2/36.png";
-// import brand_img_41 from "@/assets/img/brand/home-2/37.png";
-// import brand_img_42 from "@/assets/img/brand/home-2/38.png";
-// import brand_img_43 from "@/assets/img/brand/home-2/38.png";
-// import brand_img_44 from "@/assets/img/brand/home-2/39.png";
-// import brand_img_45 from "@/assets/img/brand/home-2/40.png";
-// import brand_img_46 from "@/assets/img/brand/home-2/41.png";
-// import brand_img_47 from "@/assets/img/brand/home-2/42.png";
-// import brand_img_48 from "@/assets/img/brand/home-2/43.png";
-// import brand_img_49 from "@/assets/img/brand/home-2/44.png";
-// import brand_img_50 from "@/assets/img/brand/home-2/45.png";
-// import brand_img_51 from "@/assets/img/brand/home-2/46.png";
-// import brand_img_52 from "@/assets/img/brand/home-2/47.png";
-// import brand_img_53 from "@/assets/img/brand/home-2/48.png";
-// import brand_img_54 from "@/assets/img/brand/home-2/49.png";
-// import brand_img_55 from "@/assets/img/brand/home-2/50.png";
-// import brand_img_56 from "@/assets/img/brand/home-2/51.png";
-// import brand_img_57 from "@/assets/img/brand/home-2/52.png";
-// import brand_img_58 from "@/assets/img/brand/home-2/53.png";
-// import brand_img_59 from "@/assets/img/brand/home-2/54.png";
-// import brand_img_60 from "@/assets/img/brand/home-2/55.png";
-// import brand_img_61 from "@/assets/img/brand/home-2/56.png";
-// import brand_img_62 from "@/assets/img/brand/home-2/57.png";
-// import brand_img_63 from "@/assets/img/brand/home-2/58.png";
-// import brand_img_64 from "@/assets/img/brand/home-2/59.png";
-// import brand_img_65 from "@/assets/img/brand/home-2/60.png";
+import brand_img_26 from "@/assets/img/brand/home-2/21.png";
+import brand_img_27 from "@/assets/img/brand/home-2/22.png";
+import brand_img_28 from "@/assets/img/brand/home-2/23.png";
+import brand_img_29 from "@/assets/img/brand/home-2/25.png";
+import brand_img_30 from "@/assets/img/brand/home-2/26.png";
+import brand_img_31 from "@/assets/img/brand/home-2/27.png";
+import brand_img_32 from "@/assets/img/brand/home-2/28.png";
+import brand_img_33 from "@/assets/img/brand/home-2/29.png";
+import brand_img_34 from "@/assets/img/brand/home-2/30.png";
+import brand_img_35 from "@/assets/img/brand/home-2/31.png";
+import brand_img_36 from "@/assets/img/brand/home-2/32.png";
+import brand_img_37 from "@/assets/img/brand/home-2/33.png";
+import brand_img_38 from "@/assets/img/brand/home-2/34.png";
+import brand_img_39 from "@/assets/img/brand/home-2/35.png";
+import brand_img_40 from "@/assets/img/brand/home-2/36.png";
+import brand_img_41 from "@/assets/img/brand/home-2/37.png";
+import brand_img_42 from "@/assets/img/brand/home-2/38.png";
+import brand_img_43 from "@/assets/img/brand/home-2/39.png";
+import brand_img_44 from "@/assets/img/brand/home-2/40.png";
+import brand_img_45 from "@/assets/img/brand/home-2/41.png";
+
+
+import shape_bg from "@/assets/img/brand/home-2/shape-bg.png";
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const paused = ref(false);
+const brandContainer = ref(null);
+
+const stopMarquee = () => {
+  paused.value = true;
+};
+
+const startMarquee = () => {
+  paused.value = false;
+};
+
+const brandImages = [
+  brand_img_1, brand_img_2, brand_img_3, brand_img_4, brand_img_5,
+  brand_img_6, brand_img_7, brand_img_8, brand_img_9, brand_img_10,
+  brand_img_11, brand_img_12, brand_img_13, brand_img_14, brand_img_15,
+  brand_img_16, brand_img_17, brand_img_18, brand_img_19, brand_img_20,
+  brand_img_21, brand_img_22, brand_img_23, brand_img_24, brand_img_25,
+  brand_img_26, brand_img_27, brand_img_28, brand_img_29, brand_img_30,
+  brand_img_31, brand_img_32, brand_img_33, brand_img_34, brand_img_35,
+  brand_img_36, brand_img_37, brand_img_38, brand_img_39, brand_img_40,
+  brand_img_41, brand_img_42, brand_img_43, brand_img_44, brand_img_45,
+];
+
+// Duplicate the brandImages array to create an extended loop
+const extendedBrandImages = brandImages.concat(brandImages);
+
 </script>
 
-
 <template>
-
   <section class="tp-brand-2-area p-relative pt-120 pb-100" @mouseover="stopMarquee" @mouseleave="startMarquee">
-    <!-- <div class="tp-section-header">
-      <h3 class="tp-section-title">Our <span class="title-color">Clients</span></h3>
-    </div> -->
     <div class="tp-brand-2-bg" :style="{ backgroundImage: `url(${shape_bg})` }"></div>
     <div class="container">
       <div class="row">
         <div class="col">
-          <div class="tp-brand-2-item" :class="{ 'paused': paused }">
-            <img class="shape-4" :src="brand_img_6" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_7" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_8" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_9" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_10" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_11" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_12" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_13" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_14" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_15" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_16" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_17" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_18" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_19" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_20" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_21" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_2" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_3" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_4" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_5" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_1" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_22" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_23" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_24" alt="image-title-here" />
-            <img class="shape-4" :src="brand_img_25" alt="image-title-here" />
+          <div class="tp-brand-2-item" :class="{ 'paused': paused }" ref="brandContainer">
+
+            <img class="shape-4" v-for="brand in extendedBrandImages" :src="brand" :key="brand" alt="brand-image" />
+
           </div>
         </div>
       </div>
@@ -127,13 +96,17 @@ import brand_img_25 from "@/assets/img/brand/home-2/20.png";
 
 <style scoped>
 .tp-brand-2-area {
+  background: var(--tp-bg-primary);
   overflow: hidden;
+  z-index: 1;
 }
 
 .tp-brand-2-item {
   display: flex;
   animation: marquee 20s linear infinite;
 }
+
+
 
 .tp-brand-2-item.paused {
   animation-play-state: paused;
